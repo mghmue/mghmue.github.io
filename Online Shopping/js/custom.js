ed3245882e37930f7dce0464c5ce4568faf92735
mgmhue <mghmue2000@gmail.com>
2025-05-04 13:19:47 +0630

    bs pj day3

---
+++ Online Shopping/js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        },{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3
+            }
+        },{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
